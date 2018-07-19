1ae79aec2d0ce0f878a299fe615aae2599cb1a62
Carl Winkler <carl@winkler.id.au>
2018-07-19 22:20:46 +0800

    Use preformatted text for commands listed in help

--- src/bot/cmd/help.ts
+++ src/bot/cmd/help.ts
@@ -3,7 +3,7 @@ import { internalRegister } from '../'
 
 internalRegister('help', `View this message`, async (bot, message, config) => {
   const cmds = getDescriptions()
-  const response = cmds.map(cmd => `*${cmd.command}*: ${cmd.desc}`).join('\n')
+  const response = cmds.map(cmd => `\`${cmd.command}\`: ${cmd.desc}`).join('\n')
   bot.postMessage({
     channel: message.channel,
     text: response,
